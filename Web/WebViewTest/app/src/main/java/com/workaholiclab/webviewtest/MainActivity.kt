d6bd29a71dbb63ead36bc54e8842dc428e015717
Gary-code <1620216938@qq.com>
2021-02-27 00:18:36 +0800

    首次更新网络博客和代码

---
+++ Web/WebViewTest/app/src/main/java/com/workaholiclab/webviewtest/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.workaholiclab.webviewtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://1.semantic-ui.com/")
+
+    }
+}
